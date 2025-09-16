6811de54ffb05f00c443cdef3d72d7197e87f9e9
Ankit Kumar Shah <ankit.shah@rtcamp.com>
2025-09-16 16:38:05 +0530

    feat: Add new blog post on the future of AI in web development

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,26 @@
-import { defineConfig } from 'astro/config';
-
-import tailwind from "@astrojs/tailwind";
-
-// Import the Vercel adapter
+import mdx from '@astrojs/mdx';
 import netlify from '@astrojs/netlify';
+import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://vitraga.netlify.app/",
-  integrations: [tailwind()],
   output: 'server',
   adapter: netlify(),
+  integrations: [
+    tailwind(),
+    sitemap(),
+    mdx({
+      syntaxHighlight: 'shiki',
+      shikiConfig: {
+        themes: {
+          light: 'github-light',
+          dark: 'github-dark',
+        },
+        wrap: true,
+      }
+    })
+  ],
 });
