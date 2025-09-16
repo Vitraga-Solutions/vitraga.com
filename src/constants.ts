8968d63ecb0f1559bccffe0dd1f99d47014b841d
Karan Poojary <karan.vitraga@gmail.com>
2025-09-16 19:34:21 +0530

    feat: Enhance Blog Page

---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+const COMPANY_NAME = "Vitraga Solutions";
+
+export { COMPANY_NAME };
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,19 @@
-import { defineCollection, z } from 'astro:content';
-const blogCollection = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    pubDate: z.date(),
-    author: z.string(),
-    authImage: z.string(),
-    image: z.string(),
-    tags: z.array(z.string()),
-    summary: z.string(),
-    type: z.enum(['Article', 'Tutorial']),
-  }),
+import { defineCollection, z } from "astro:content";
+
+const postsCollection = defineCollection({
+	schema: ({image}) => z.object({
+		title: z.string(),
+		coverImage: image(),
+		tags: z.array(z.string()),
+		featured: z.boolean().default(false),
+		excerpt: z.string().optional(),
+		publishedDate: z.date().transform((date) => date
+			.toLocaleDateString(undefined, {
+				year: "numeric",
+				month: "short",
+				day: "numeric",
+		})),
+	}),
 });
 
 const serviceCollection = defineCollection({
@@ -22,6 +25,6 @@ const serviceCollection = defineCollection({
 });
 
 export const collections = {
-  blog: blogCollection,
-  service: serviceCollection,
+	blogs: postsCollection,
+	service: serviceCollection,
 };
